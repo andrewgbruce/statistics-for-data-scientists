77f4db0f15395c30a7a388a7f6ea882c7c2ce90d
Andrew Bruce <andrew@dato.com>
2017-06-17 13:48:24 -0700

    Initial scripts organized by chapter

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # statistics-for-data-scientists
-Code and data associated with the book "Statistics for Data Scientists: 50 Essential Concepts"
+Code associated with the book "Practical Statistics for Data Scientists: 50 Essential Concepts"
+
+The scripts are stored by chapter and replicate most the figures and code snippets.
+The data is not saved on github and you will need to download the data using the sript src/download_data.r. 
+This will copy the data into the data directory
+
+IMPORTANT NOTE: 
+The scripts all assume that you have clone the repository into the top level directory.
+If you save the repository elsewhere, you will need to edit the line
+
+  PSDS_PATH <- file.path('~', 'statistics-for-data-scientists')
+
+to point ot the appropriate directory in all of the scripts.
---
+++ src/download_data.r
@@ -0,0 +1,61 @@
+library(RCurl)
+PSDS_PATH <- file.path('~', 'statistics-for-data-scientists')
+
+download_from_google_drive <- function(id, fname, path)
+{
+  url <- sprintf("https://drive.google.com/uc?export=download&id=%s", id)
+  data <- getBinaryURL(url, followlocation = TRUE, ssl.verifypeer = FALSE)
+  dest <- file.path(path, 'data', fname)
+  writeBin(data, dest, useBytes = TRUE)
+}
+
+## Import state data
+download_from_google_drive(id="0B98qpkK5EJembFc5RmVKVVJPdGc", fname='state.csv', path=PSDS_PATH)
+
+## Airline data
+download_from_google_drive(id="0B98qpkK5EJemcmZYX2VhMHBXelE", fname='dfw_airline.csv', path=PSDS_PATH)
+
+## Import stock data
+download_from_google_drive('0B98qpkK5EJemV2htZWdhVFRMNlU', fname='sp500_px.csv', path=PSDS_PATH)
+download_from_google_drive('0B98qpkK5EJemY0U0N1N6a21lUzA', fname='sp500_sym.csv', path=PSDS_PATH)
+
+## Import KC housing tax data
+download_from_google_drive('0B98qpkK5EJemck5VWkszN3F3RGM', fname='kc_tax.csv', path=PSDS_PATH)
+
+## Import lending club loan data
+download_from_google_drive('0B98qpkK5EJemRXpfa2lONlFRSms', fname='lc_loans.csv', path=PSDS_PATH)
+
+## Import a sample of 200 records from lending club loan data
+download_from_google_drive('0B98qpkK5EJemd0JnQUtjb051dTA', fname='loan200.csv', path=PSDS_PATH)
+
+## Import a sample of 3000 records from lending club loan data
+download_from_google_drive('0B98qpkK5EJemQXYtYmJUVkdsN1U', fname='loan3000.csv', path=PSDS_PATH)
+
+
+## Import a complete set of records from lending club loan data
+download_from_google_drive('0B98qpkK5EJemZzdoQ2I3SWlBYzg', fname='loan_data.csv', path=PSDS_PATH)
+
+## Import loans income data
+download_from_google_drive('0B98qpkK5EJemRXVld0NSbWhYNVU', fname='loans_income.csv', path=PSDS_PATH)
+
+## Import session_times data
+download_from_google_drive('0B98qpkK5EJemOC0xMHBTTEowYzg', fname='web_page_data.csv', path=PSDS_PATH)
+
+## Import four_sessions data
+download_from_google_drive('0B98qpkK5EJemOFdZM1JsaEF0Mnc', fname='four_sessions.csv', path=PSDS_PATH)
+
+## Import click_rate data
+download_from_google_drive('0B98qpkK5EJemVHB0ZzdtUG9SeTg', fname='click_rates.csv', path=PSDS_PATH)
+
+## Import imanishi data
+download_from_google_drive('0B98qpkK5EJemZTJnUDd5Ri1vRDA', fname='imanishi_data.csv', path=PSDS_PATH)
+
+## Import lung disease data
+download_from_google_drive('0B98qpkK5EJemb25YYUFJZnZVSnM', fname='LungDisease.csv', path=PSDS_PATH)
+
+## Import Zillow's county level ZHVI 
+download_from_google_drive('0B98qpkK5EJemWGRWOEhYN1RabVk', fname='County_Zhvi_AllHomes.csv', path=PSDS_PATH)
+
+## Import King county house sales data
+download_from_google_drive('0B98qpkK5EJemVTRRN0dLakxwTmM', fname='house_sales.csv', path=PSDS_PATH)
+
